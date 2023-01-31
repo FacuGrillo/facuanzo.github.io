427e17a351ef1f488bbd5043edd8ae85c7192a06
FacuGrillo <facundoanzoategui@outlook.com>
2023-01-31 12:56:03 -0300

    proyectocv

---
+++ funciones/functions.js
@@ -0,0 +1,32 @@
+document.getElementById('mail_icono').addEventListener('mouseover', function() {
+    document.getElementById('mail').style.display = 'block';
+});
+
+document.getElementById('mail_icono').addEventListener('mouseout', function() {
+    document.getElementById('mail').style.display = 'none';
+});
+
+document.getElementById('fecha_nacimiento_icono').addEventListener('mouseover', function() {
+    document.getElementById('fecha_nacimiento').style.display = 'block';
+});
+
+document.getElementById('fecha_nacimiento_icono').addEventListener('mouseout', function() {
+    document.getElementById('fecha_nacimiento').style.display = 'none';
+});
+
+document.getElementById('direccion_icono').addEventListener('mouseover', function() {
+    document.getElementById('direccion').style.display = 'block';
+});
+
+document.getElementById('direccion_icono').addEventListener('mouseout', function() {
+    document.getElementById('direccion').style.display = 'none';
+});
+
+document.getElementById('telefono_icono').addEventListener('mouseover', function() {
+    document.getElementById('telefono').style.display = 'block';
+});
+
+document.getElementById('telefono_icono').addEventListener('mouseout', function() {
+    document.getElementById('telefono').style.display = 'none';
+});
+
